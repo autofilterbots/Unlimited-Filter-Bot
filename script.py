b0d8ce777584fe43d0140404250fc6acddc08ad5
autofilterbots <88230058+autofilterbots@users.noreply.github.com>
2021-08-08 04:23:07 +0530

    Update script.py

--- script.py
+++ script.py
@@ -52,7 +52,7 @@ See <i>/help</i> for commands and more details.</b>
 """
 
 
-    ABOUT_MSG = """⭕️<b>My Name : MT Unlimited Filter Bot</b>
+    ABOUT_MSG = """⭕️<b>My Name : Vj Unlimited Filter Bot</b>
 
 ⭕️<b>Creater :</b> <b>@Achu_vjsvj</b>    
 
